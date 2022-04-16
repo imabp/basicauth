95ed5da515d6d839f764793079fdd1512485ed5f
Abir <abir.pal899@gmail.com>
2022-04-16 15:19:03 +0530

    fix: add boilerplate code

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,16 +1,17 @@
 import type { NextPage } from 'next'
 
-const Home: NextPage = () => {
+const Login: NextPage = () => {
   return (<>
     <div
       className='flex flex-row w-full justify-center'
     >
+
       <div className='m-5 flex-col'>
         <h1 className="text-xl font-bold text-center">
           Basic Authentication Demo
         </h1>
           <div className='max-w-xs mt-5'>
-            Signup Component Goes Here
+            Login Component Goes Here
           </div>
       </div>
     </div>
@@ -19,4 +20,4 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export default Login
